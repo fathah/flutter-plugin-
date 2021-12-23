27935dbcf74c3f583cc311dfd0f112942fcbc786
Fathah Cr <48355244+fathah@users.noreply.github.com>
2021-12-23 20:33:43 +0530

    Added Name Parameter

--- lib/smart_pos_z91_interface.dart
+++ lib/smart_pos_z91_interface.dart
@@ -7,8 +7,8 @@ class SmartPosZ91Interface {
   static const MethodChannel _channel = MethodChannel('smart_pos_z91_interface');
   static const MethodChannel _channel1 = MethodChannel('name');
 
-  static Future<String?> get print async {
-    final String? printStatus = await _channel.invokeMethod('print');
+  static Future<String?> print(name) async {
+    final String? printStatus = await _channel.invokeMethod('print', {'name':name});
     return printStatus;
   }
 }
